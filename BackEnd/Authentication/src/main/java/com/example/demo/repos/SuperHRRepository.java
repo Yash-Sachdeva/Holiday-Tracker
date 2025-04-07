6134898d35d636b86dc7fb5570247a4d00c807bd
amanwissen15 <Aman.Joshi@wissen.com>
2025-04-07 21:31:15 +0530

    Removed SuperHR

--- BackEnd/Authentication/src/main/java/com/example/demo/repos/SuperHRRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entities.SuperHR;
-
-public interface SuperHRRepository extends JpaRepository<SuperHR,Long>{
-	SuperHR findByEmail(String email);
-}
